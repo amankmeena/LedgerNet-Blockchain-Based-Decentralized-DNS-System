73ebb26e6c22020cdae618d9f0c0df17194061ed
Aman Meena <121188626+amankmeena@users.noreply.github.com>
2025-11-21 18:06:28 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 const { ethers } = require("hardhat");
-const fs = require("fs"); // file handle here
+const fs = require("fs"); // file handle
 const path = require("path");
 
 async function main() {
